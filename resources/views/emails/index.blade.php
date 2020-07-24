54310bb4c240258ccdfe718bb58a1c0daf1df21f
felipe0909 <felipewin48@gmail.com>
2020-07-23 21:42:51 -0300

    alterando url de produção

--- resources/views/emails/index.blade.php
+++ resources/views/emails/index.blade.php
@@ -2,7 +2,7 @@
     <h2>Olá {{ $user->name }},</h2>
     <h3>Clique no botão abaixo para realizar o download do relatório solicitado.</h3>
     @component('mail::button', [
-    'url' =>  'http://localhost:8000/download/file/'.$file->name.'?token=106c0939-9d67-4928-a909-27fdbf4d65be'
+    'url' =>  'https://app.safed.com.br/download/file/'.$file->name.'?token=106c0939-9d67-4928-a909-27fdbf4d65be'
     ])
     Download
     @endcomponent
