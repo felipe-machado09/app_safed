643aecd4584d2ec6d6b8a1e8b394202339578569
felipe0909 <felipewin48@gmail.com>
2020-07-25 13:36:23 -0300

    update de usuario com o mesmo email

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -26,7 +26,7 @@ class UserRequest extends FormRequest
     {
         return [
           "name" => "required|unique:users",
-          "email" => 'required|email',Rule::unique('users','email')->ignore($this->user->id),
+          "email" => 'required|email',Rule::unique('users','email')->ignore(request('user')),
           "password" => "required"
         ];
     }
